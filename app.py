319ad7f34a2b0a53b7aee33c2acc87b6e126e4ce
bahadir <bhakyuz@gmail.com>
2020-02-16 14:25:19 +0100

    Parse url params and put them in pd dataframe

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask import request
+import pandas as pd
+
+
 app = Flask(__name__)
 api = Api(app)
 
+# Example 
+# http://127.0.0.1:8000/?text=tata&toto=toktok&a=2
+
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
-
+        url_params = request.args
+        df = pd.DataFrame.from_records(url_params, index=[0])
+        return {'url_params': url_params, 'df': df.to_dict()}
+        
 api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
